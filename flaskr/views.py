4106c20089e80ebe5d124c0505434b613babab96
petrstipek <stip05@vse.cz>
2024-02-12 22:35:46 +0100

    Setup of views for web page.

--- flaskr/views.py
+++ flaskr/views.py
@@ -3,5 +3,29 @@ from flask import Blueprint
 views = Blueprint("views", __name__)
 
 @views.route('/')
-def hello():
-    return 'Hello, World!'
+def main_page():
+    return "<h1>Main reservation page</h1>"
+
+@views.route('/reservation-check')
+def reservation_check():
+    return "<h1>Reservation check page</h1>"
+
+@views.route('/reservations-user')
+def reservations_user():
+    return "<h1>Reservations from user</h1>"
+
+@views.route('/login-page-admin')
+def login_page_admin():
+    return "<h1>Login page admin</h1>"
+
+@views.route('/admin-page')
+def admin_page():
+    return "<h1>Admin page</h1>"
+
+@views.route('/lectures')
+def lectures():
+    return "<h1>Lectures page</h1>"
+
+@views.route('/reservations-admin')
+def reservations_admin():
+    return "<h1>Reservations admin page</h1>"
