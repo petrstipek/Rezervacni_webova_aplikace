b8e9d5d7d4f54990781a162021f34a8dfe7485e9
petrstipek <stip05@vse.cz>
2024-02-16 21:37:10 +0100

    script and css split form html - calendar logic adjustments

---
+++ flaskr/static/js/reservation.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    $('#same_as_student_checkbox').change(function () {
+        if (this.checked) {
+            $('#student_details_section').show();
+        } else {
+            $('#student_details_section').hide();
+        }
+    });
+
+    // Parse the availableTimes data from the data-available-times attribute
+    console.log("vypis tady");
+    console.log($('#datepicker').data('available-times'))
+    var availableTimes = ($('#datepicker').data('available-times'));
+    console.log(availableTimes);
+    var selectedDate = ''; // Store the selected date in a broader scope
+
+    $('#datepicker').datepicker({
+        dateFormat: 'mm/dd/yy',
+        onSelect: function (dateText) {
+            selectedDate = dateText; // Update selectedDate when a date is picked
+            var times = availableTimes[dateText] || [];
+            var timesHtml = times.map(function (time) {
+                return `<label><input type="checkbox" name="time" value="${time}" /> ${time}</label><br>`;
+            }).join('');
+
+            $('.times-container').html(timesHtml);
+        }
+    });
+
+    // Event delegation for dynamically added checkboxes
+    $('.times-container').on('change', 'input[name="time"]', function () {
+        if (this.checked) {
+            // Prepare the data to be sent to the server
+            var dataToSend = {
+                date: selectedDate,
+                time: $(this).val()
+            };
+
+            // Send the data to the server using AJAX
+            $.ajax({
+                type: "POST",
+                url: "/submit-date-time",
+                contentType: "application/json",
+                data: JSON.stringify(dataToSend),
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("X-CSRFToken", $("input[name='csrf_token']").val());
+                },
+                success: function (response) {
+                    console.log("Data sent successfully", response);
+                },
+                error: function (xhr, status, error) {
+                    console.error("Error sending data", error);
+                }
+            });
+        }
+    });
+});
